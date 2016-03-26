46af49f5f22e9f513bbbe7153843d7114ed1fa5f
_knaut <info@knaut.net>
2016-03-26 13:06:35 -0700

    simple osc heartbeat

---
+++ app.js
@@ -0,0 +1,36 @@
+var osc = require('osc-min');
+var dgram = require('dgram');
+
+var udp = dgram.createSocket('udp4');
+
+// console.log(udp);
+
+var outport;
+if (process.argv[2]) {
+	outport = parseInt(process.argv[2]);
+} else {
+	outport = 41234;
+}
+
+// console.log(outport);
+
+
+
+var sendHeartbeat = function() {
+	var buf = osc.toBuffer({
+		address: '/hearbeat',
+		args: [
+			12,
+			'stringingnging',
+			new Buffer('beat', {
+				type: 'integer',
+				value: 7
+			})
+		]
+	})
+
+	udp.send(buf, 0, buf.length, outport, 'localhost');
+	console.log(buf)
+};
+
+setInterval(sendHeartbeat, 2000);
